bc7695924270b7154e31b76e00ad6ffd79f60cb8
Eilidh Morone <e.silendt@gmail.com>
2022-12-17 21:53:48 +0000

    added run_sql import, and added update function to stylist_repository

--- controllers/stylists_controller.py
+++ controllers/stylists_controller.py
@@ -1,3 +1,4 @@
+from db.run_sql import run_sql
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.stylist import Stylist
@@ -9,3 +10,8 @@ stylists_blueprint = Blueprint("stylists", __name__)
 def stylists():
     stylists = stylist_repository.select_all()
     return render_template("stylists/index.html")
+
+def update(stylist):
+    sql = "UPDATE stylists SET name = %s WHERE id = %s"
+    values = [stylist.name, stylist.phone_number,stylist.id]
+    run_sql(sql, values)
