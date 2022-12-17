696f70e8f71ba941bb261cb3aa8e6ce0fa6b9f2e
Eilidh Morone <e.silendt@gmail.com>
2022-12-17 21:44:52 +0000

    added /clients/new route to clients_controller.py

--- controllers/clients_controller.py
+++ controllers/clients_controller.py
@@ -5,8 +5,14 @@ import repositories.client_repository as client_repository
 
 clients_blueprint = Blueprint("clients", __name__)
 
+#Index
 @clients_blueprint.route("/clients")
 def stylists():
     clients = client_repository.select_all()
     return render_template("clients/index.html")
 
+
+#New
+@clients_blueprint.route("/clients/new")
+def new_client():
+    return render_template("humans/new.html")
