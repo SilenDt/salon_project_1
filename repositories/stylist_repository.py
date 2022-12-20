c86798de5e046a3423980ebc7a9a3a574d61a9c2
Eilidh Morone <e.silendt@gmail.com>
2022-12-20 10:28:53 +0000

    added delete button to stylist/index.html and fixed a syntax eror in stylist_repository that was not allowing the delete button to function correctly

--- repositories/stylist_repository.py
+++ repositories/stylist_repository.py
@@ -32,7 +32,7 @@ def delete_all():
     run_sql(sql)
 
 def delete(id):
-    sql = "DELETE FROM stylists WHERE is = %s"
+    sql = "DELETE FROM stylists WHERE id = %s"
     values = [id]
     run_sql(sql, values)
 
