94586e9f0e92e5a0930a164da9f263a22fcb28f3
Eilidh Morone <e.silendt@gmail.com>
2022-12-20 14:15:41 +0000

    added create_appointment function in appointments_controller.py

--- controllers/appointments_controller.py
+++ controllers/appointments_controller.py
@@ -8,15 +8,28 @@ import repositories.stylist_repository as stylist_repository
 
 appointments_blueprint = Blueprint("appointments", __name__)
 
-#INDEX
+# INDEX
 @appointments_blueprint.route("/appointments")
 def appointments():
     appointments = appointment_repository.select_all()
     return render_template("appointments/index.html", appointments=appointments)
 
-#New
+# NEW
 @appointments_blueprint.route("/appointments/new")
 def new_appointment():
     clients = client_repository.select_all()
     stylists = stylist_repository.select_all()
     return render_template("appointments/new.html", clients=clients, stylists=stylists)
+
+# CREATE
+@appointments_blueprint.route("/appointments", methods=["POST"])
+def create_appointment():
+    client_id = request.form["client_id"]
+    stylist_id = request.form["stylist_id"]
+    date_time = request.form["date_time"]
+    client = client_repository.select(client_id)
+    stylist = stylist_repository.select(stylist_id)
+    date_time = appointment_repository.select("date_time")
+    new_appointment = Appointment(client, stylist, date_time)
+    appointment_repository.save(new_appointment)
+    return redirect("/appointments")
